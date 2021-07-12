b49a2fcebcc5bf624d3fafa6ddbe17bb06863776
Aaryanverma <aryan212verma@gmail.com>
2021-07-12 14:23:34 +0530

    updated translation code

--- google_trans_new.py
+++ google_trans_new.py
@@ -87,7 +87,7 @@ class google_translator:
 
     '''
 
-    def __init__(self, url_suffix="cn", timeout=5, proxies=None):
+    def __init__(self, url_suffix="en", timeout=5, proxies=None):
         self.proxies = proxies
         if url_suffix not in URLS_SUFFIX:
             self.url_suffix = URL_SUFFIX_DEFAULT
